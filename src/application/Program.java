812e79ff3863dd4abb3a90426d449c6d6a4dadb8
Weslley Alberto <weslleyalberto@gmail.com>
2022-11-07 19:51:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args){
        Department obj = new Department(1,"Books");
+       Seller seller = new Seller(1,"Weslley","email@gmail.com",new Date(),3000.99, obj);
        System.out.println(obj);
+       System.out.println(seller);
     }
 }
